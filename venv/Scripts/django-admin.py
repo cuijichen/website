36c48b71b2d377a4518d4616de8c144108aaf529
cuijichen <2805647543@qq.com>
2020-09-17 11:22:31 +0800

    第一次

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Administrator\Desktop\2020\2020.9\website\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
